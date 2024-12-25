e0c1bc478a634010d4b70384fe8d379322cc6978
SantiagoBer13 <bernaltinjacasantiago113@gmail.com>
2024-12-25 13:04:56 -0500

    feat: Added method to show menu

--- src/App.java
+++ src/App.java
@@ -65,4 +65,14 @@ public class App {
 
     
   }
+
+  public static void showMenu() {
+    System.out.println("==============================");
+    System.out.println("      MENU PRINCIPAL");
+    System.out.println("==============================");
+    System.out.println("1. Seleccionar un planeta de destino.");
+    System.out.println("2. Seleccionar una nave espacial.");
+    System.out.println("3. Iniciar simulacion de viaje.");
+    System.out.println("4. Salir.");
+  }
 }
